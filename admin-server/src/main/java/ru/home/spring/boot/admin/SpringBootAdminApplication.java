cb4610a2e766d1f0649d62571dbc477ca55d138b
Zaets Dmitriy <diemon89@gmail.com>
2017-02-02 21:08:57 +0300

    Изменение версий и их согласованность. Форматирование файлов. Добавлен пустой модуль.

--- admin-server/src/main/java/ru/home/spring/boot/admin/SpringBootAdminApplication.java
+++ admin-server/src/main/java/ru/home/spring/boot/admin/SpringBootAdminApplication.java
@@ -1,11 +1,10 @@
 package ru.home.spring.boot.admin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-
-import de.codecentric.boot.admin.config.EnableAdminServer;
 
 
 @Configuration
@@ -13,7 +12,7 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 @EnableDiscoveryClient
 @EnableAdminServer
 public class SpringBootAdminApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
 }
